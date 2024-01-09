8e3ea8fba8fdb65d5c877955b2240b670c830610
szelezsan <147345487+szelezsan@users.noreply.github.com>
2024-01-09 15:00:07 +0000

    added Javascrip for starting the game and for buttons to work

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+/* Starting game */
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
 
+const questionElement = document.getElementById('question')
+const answerButtonsElement= document.getElementById('answer-buttons')
+
+
+/* Shuffling question, so they will be presented in different orders each time */
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+    startButton.classList.add('hide')
+    /*Shuffling questions in random orders*/
+    shuffledQuestions = questions.sort(() => Math.random()- .5)
+    currentQuestionIndex= 0
+    questionContainerElement.classList.remove('hide')
+    /* Setting up showing next question */
+    setNextQuestion()
+}
+
+function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer(){
+
+}
+
+/* List of questions */
+const questions = [
+    {
+        question: 'What is the best selling album of all time from 1976 to 2018';
+        answers: [
+            {text: 'Thrillers', correct: true}
+            {text: 'Back in Black', correct: false}
+            {text: 'Abbey Road', correct: false}
+        ]
+    }
+]
