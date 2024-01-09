2672f51410a9802648820eb5dfb6ccf9bdb2caea
szelezsan <147345487+szelezsan@users.noreply.github.com>
2024-01-09 15:30:54 +0000

    Addded essential javascript to game

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* Starting game */
 const startButton = document.getElementById('start-btn')
+const nextButton =document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
-
 const questionElement = document.getElementById('question')
 const answerButtonsElement= document.getElementById('answer-buttons')
 
@@ -10,6 +10,10 @@ const answerButtonsElement= document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', ()=> {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame(){
     startButton.classList.add('hide')
@@ -22,25 +26,63 @@ function startGame(){
 }
 
 function setNextQuestion(){
+    resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button= dosument.createElement('button')
+        button.innerText= answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct= answer.correct
+    }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild){
+        (answerButtonsElement.removeChild(answerButtonsElement.firstChild))
 }
 
 function selectAnswer(){
+    const selectButton= e.target
+    const correct=  selectedButton.dataset.correct
+    setStatus(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.lenght > currentQuestionIndex +1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText= 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
 
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
 }
 
 /* List of questions */
 const questions = [
     {
-        question: 'What is the best selling album of all time from 1976 to 2018';
+        question: 'What is the best selling album of all time from 1976 to 2018',
         answers: [
-            {text: 'Thrillers', correct: true}
-            {text: 'Back in Black', correct: false}
+            {text: 'Thrillers', correct: true},
+            {text: 'Back in Black', correct: false},
             {text: 'Abbey Road', correct: false}
         ]
     }
-]
+]}
