aa2bfc88942bdc783f461fc27cbfd3eb6b1b8669
szelezsan <147345487+szelezsan@users.noreply.github.com>
2024-01-09 13:42:10 +0000

    Changed css to customize gane

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const quizfrom = document.getElementById('quiz-form');
-const resultDiv = document.getElementById('result');
 
-const correctAnswers = ['a', 'b', 'b', 'a', 'b', 'c', 'b', 'a', 'c', 'c']
-
-quizfrom.addEventListener('submit, e=>'); {
-    e.preventDefault();
-
-    let score = 0;
-    const.userAnswers =[quizfrom.q1.value,quizFrom.q2.value]
-    userAnswers.forEach(answer, index) ; {
-        if (answer === correctAnswers[index]){
-        score += 1;
-        }
-    };
-    resultDiv.dispatchEvent.innerHTML= 'Your score is ${score}/${correctAnswers.length}';
-})
