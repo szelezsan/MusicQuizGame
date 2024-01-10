0af6eb5600b276740798a15b0902f777e86589d9
szelezsan <147345487+szelezsan@users.noreply.github.com>
2024-01-10 09:26:12 +0000

    changed const to var for start btn

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /* Starting game */
-const startButton = document.getElementById('start-btn')
+var startButton = document.getElementById('start-btn')
 const nextButton =document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
