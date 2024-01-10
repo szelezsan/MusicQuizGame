af57f483d0b5e05eafee1c75d2af4630c9b5a14e
szelezsan <147345487+szelezsan@users.noreply.github.com>
2024-01-10 11:13:26 +0000

    Declared selectAnswer and corrected typos is JS

--- script.js
+++ script.js
@@ -1,3 +1,87 @@
+/* List of questions */
+const questions = [
+    {
+        question: 'What is the best selling album of all time from 1976 to 2018',
+        answers: [
+            {text: 'Thrillers', correct: true},
+            {text: 'Back in Black', correct: false},
+            {text: 'Abbey Road', correct: false}
+        ]
+    },
+    {
+        question: 'Who is the lead singer of the British pop rock band Coldplaye from 1976 to 2018',
+        answers: [
+            {text: 'Guy BerryMan', correct: false},
+            {text: 'Chris Martin', correct: true},
+            {text: 'Will Champion', correct: false}
+        ]
+    },
+    {
+        question: 'Who is the frontman of the band 30 Seconds to Mars?',
+        answers: [
+            {text: 'Mike Shinoda', correct: false},
+            {text: 'Gerard Way', correct: false},
+            {text: 'Jared Leto', correct: true}
+        ]
+    },
+    {
+        question: 'The band Muse released their first album, Showbiz, in what year?',
+        answers: [
+            {text: '2001', correct: false},
+            {text: '2002', correct: false},
+            {text: '1999', correct: true}
+        ]
+    },
+    {
+        question: 'Which rap group released the album "Straight Outta Compton"?',
+        answers: [
+            {text: 'Beastie Boys', correct: false},
+            {text: 'Run-D.M.C.', correct: false},
+            {text: 'N.W.A', correct: true},
+        ]
+    },
+    {
+        question: '"Some people call me the space cowboy" is the first line from what song?',
+        answers: [
+            {text: 'Fly like an Eagle', correct: false},
+            {text: 'The Joker', correct: true},
+            {text: 'Fandago', correct: false}
+        ]
+    },
+    {
+        question: 'Who was featured in the song "Words" by Feint?',
+        answers: [
+            {text: 'Anna Yvette', correct: false},
+            {text: 'Veela', correct: false},
+            {text: 'Laura Brehm', correct: true}
+        ]
+    },
+    {
+        question: 'From which country did the song "Gangnam Style" originate from?',
+        answers: [
+            {text: 'South Korea', correct: true},
+            {text: 'North Korea', correct: false},
+            {text: 'China', correct: false}
+        ]
+    },
+    {
+        question: 'Which 80s band is fronted by singer/guitarist Robert Smith?',
+        answers: [
+            {text: 'New Order', correct: false},
+            {text: 'The Cure', correct: true},
+            {text: 'The Smiths', correct: false}
+        ]
+    },
+    {
+        question: 'Which group performs the song "Crash into Me"',
+        answers: [
+            {text: 'Phish', correct: false},
+            {text: 'The Greatful Dead', correct: false},
+            {text: 'Dave Matthews Band', correct: true}
+        ]
+    },
+]
+
 /* Starting game */
 var startButton = document.getElementById('start-btn')
 const nextButton =document.getElementById('next-btn')
@@ -46,6 +130,11 @@ function showQuestion(question) {
     })
 }
 
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
+
 function resetState() {
     clearStatusClass(document.body)
     nextButton.classList.add('hide')
@@ -53,18 +142,18 @@ function resetState() {
         (answerButtonsElement.removeChild(answerButtonsElement.firstChild))
 }
 
-function selectAnswer(){
+function selectAnswer(e) {
     const selectedButton= e.target
     const correct=  selectedButton.dataset.correct
     setStatus(document.body, correct)
     Array.from(answerButtonsElement.children).forEach(button => {
         setStatusClass(button, button.dataset.correct)
-    })
+    });
     if (shuffledQuestions.length > currentQuestionIndex +1) {
-        nextButton.classList.remove('hide')
+        nextButton.classList.remove('hide');
     } else {
-        startButton.innerText= 'Restart'
-        startButton.classList.remove('hide')
+        startButton.innerText= 'Restart';
+        startButton.classList.remove('hide');
     }
 }
 
@@ -76,87 +165,4 @@ function setStatusClass(element, correct) {
         element.classList.add('wrong')
     }
 }
-
-/* List of questions */
-const questions = [
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-    {
-        question: 'What is the best selling album of all time from 1976 to 2018',
-        answers: [
-            {text: 'Thrillers', correct: true},
-            {text: 'Back in Black', correct: false},
-            {text: 'Abbey Road', correct: false}
-        ]
-    },
-]}
+}
